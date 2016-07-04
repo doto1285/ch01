6cf5d8a0ebdeda059f0ba63a45d2e0b563f8ac50
donghyo <eotkdehwnd12@naver.com>
2016-07-04 15:47:50 +0900

    1st commit

---
+++ src/ch01/StudentTest.java
@@ -0,0 +1,12 @@
+package ch01;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.name = "a";
+		System.out.println(student.name);
+
+	}
+
+}
